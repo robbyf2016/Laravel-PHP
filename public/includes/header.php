8112f13196228514d67f6113405c1340854a90d2
Robby Fussell <robbyfussell@fas.harvard.edu>
2014-12-18 10:39:48 -0500

    First Commit

---
+++ public/includes/header.php
@@ -0,0 +1,6 @@
+<!--*************************************************************-->
+<!--Header section for code reuse and consistency.               -->
+<!--*************************************************************-->
+<div class="leftcolumn"><img id="hackerimage" src="images/hacker.png" height="50" width="50" /></div>
+<div class="centercolumn"><h1 id="headertitle">Robby Fussell - Harvard Coding Page</h1></div>
+<div class="rightcolumn"><img id="harvardme" src="images/myHarvardLogo.png" width="75" height="75"  /></div>
