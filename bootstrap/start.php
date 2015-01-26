6802990fd52ffa3fd38ae01f5af11d8c9b35861c
Robby Fussell <robbyfussell@fas.harvard.edu>
2015-01-26 14:49:01 -0500

    Added production and local env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('robbyfussell-harvard'),
+	'local' => array('Robbys-Air.home'),
+	'production' => array('robbyfussell-harvard'),
 
 ));
 
